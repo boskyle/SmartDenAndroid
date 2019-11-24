715f4d1b7b7533387c052e9ccb161665d8d7cf51
Sam <ftgs0002@humbermail.ca>
2019-11-24 00:35:38 -0500

    --Back end set up but hav not made connection to database

--- SmartDenProject/SmartDen/src/main/java/smartden/project/Registration.java
+++ SmartDenProject/SmartDen/src/main/java/smartden/project/Registration.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Registration extends AppCompatActivity {
@@ -16,7 +31,9 @@ public class Registration extends AppCompatActivity {
     EditText email;
     EditText password;
     EditText fullname;
-
+    RequestQueue requestqueue;
+    String URL= "https://apollo.humber.ca/~ftgs0002/Register.php";
+    StringRequest request;
     public static boolean isValid(String email)
     {
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
@@ -35,7 +52,7 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
+        requestqueue = Volley.newRequestQueue(this);
         TextView tv_reg = (TextView)findViewById(R.id.lnkLogin);
         tv_reg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,8 +60,10 @@ public class Registration extends AppCompatActivity {
 
                 Intent intent = new Intent(Registration.this, Log_in.class);
                 startActivity(intent);
+
             }
         });
+
 
         final Button button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +102,36 @@ public class Registration extends AppCompatActivity {
                 }
             }
         });
+        request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                }
+                catch(JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.d("error",error.toString());
+            }
+        }){
+            protected Map<String,String> getParams() throws AuthFailureError{
+                HashMap<String,String> hashmap = new HashMap <String,String>();
+                hashmap.put("Email",email.getText().toString());
+                hashmap.put("Name",fullname.getText().toString());
+                hashmap.put("Password",password.getText().toString());
+
+                return hashmap;
+            }
+
+        };
+    requestqueue.add(request);
+
 
 
     }
